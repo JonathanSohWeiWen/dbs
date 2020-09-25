350fb0f6058086a5a789d86703580b3bc38904c3
Taufiq <taufiq.sept@gmail.com>
2020-09-25 12:31:33 +0800

    custName form

--- src/FORM/form.js
+++ src/FORM/form.js
@@ -2,24 +2,34 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 
-function Form() {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js HELLO</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
+class Form extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+      custName: null,
+    };
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.custName});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.custName);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.mySubmitHandler}>
+        <h1>Please enter your data</h1>
+        <p>Enter your name:</p>
+        <input type="text" value={this.state.custName} onChange={this.handleChange} />
+      </form>
+    );
+  }
 }
+
+
 
 export default Form;
