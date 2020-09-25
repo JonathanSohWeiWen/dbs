b8bea9f7a0961aae3f64878062eaf4d8373537e6
bernardlimky <64117839+bernardlimky@users.noreply.github.com>
2020-09-25 13:04:43 +0800

    Update form.js
    Adding the require inputs

--- src/FORM/form.js
+++ src/FORM/form.js
@@ -6,7 +6,12 @@ class Form extends React.Component {
 	constructor(props) {
     super(props);
     this.state = {
-      custName: '',
+	  custName: '',
+	  custAge: '',
+	  offName: '',
+	  custNric: '',
+	  curTime : new Date().toLocaleString(),
+	  branchCode: '',
     };
   }
 
@@ -20,12 +25,40 @@ class Form extends React.Component {
     return (
       <form onSubmit={this.mySubmitHandler}>
         <h1>Please enter your data</h1>
-        <p>Enter your name:</p>
+        <p>Full Name:</p>
         <input
           type='text'
           name='custName'
           onChange={this.myChangeHandler}
         />
+		<p>Age:</p>
+        <input
+          type='integer'
+          name='custAge'
+          onChange={this.myChangeHandler}
+        />
+		<p>Service Officer Name:</p>
+        <input
+          type='text'
+          name='offName'
+          onChange={this.myChangeHandler}
+        />
+		<p>NRIC:</p>
+        <input
+          type='text'
+          name='custNric'
+          onChange={this.myChangeHandler}
+        />
+		<p>Register date and time (DD/MM/YYYY HH.MM.SS):</p>
+		<p>Now time: {this.state.curTime}</p>
+        <p>Branch Code:</p>
+        <input
+          type='integer'
+          name='branchCode'
+          onChange={this.myChangeHandler}
+        />
+		<p>Image:</p>
+		<p>Product Type:</p>
       </form>
     );
   }
